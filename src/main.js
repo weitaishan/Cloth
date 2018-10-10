3b5405b4c784555cec66ac12324819e3d877601d
魏太山 <450785730@qq.com>
2018-10-10 13:18:31 +0800

    add element-ui and axios

--- src/main.js
+++ src/main.js
@@ -2,8 +2,19 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+import axios from 'axios';
+
+Vue.use(ElementUI);
 
 Vue.config.productionTip = false
+
+Vue.prototype.$axios = axios;
+
+// axios.defaults.baseURL ="http://101.251.70.142:8888/";
+// axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded;charset=UTF-8';
+// axios.defaults.timeout = 5000;
 
 new Vue({
   router,
